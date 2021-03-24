2474c6ff342872126a933658e916e1429f97965b
melody2005 <55598373+melody2005@users.noreply.github.com>
2021-03-24 12:35:39 -0400

    Add files via upload
    Added Varaibles

---
+++ Varaibles.py
@@ -0,0 +1,10 @@
+# Add your Python code here. E.g.
+from microbit import *
+
+#_____=______
+
+answer = 42
+hello_message = "Hello, there!"
+display.scroll(hello_message)
+hello_message = "Yo"
+display.scroll(hello_message)
